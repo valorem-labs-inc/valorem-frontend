84c6ebac4d6e7ae464096adcd2638d8df60b3c59
thal0x <thal0x.eth@gmail.com>
2022-07-13 11:36:41 -0500

    refactor vault layout styles

--- pages/vault/index.tsx
+++ pages/vault/index.tsx
@@ -1,29 +1,38 @@
 import { NextPage } from "next";
 import dynamic from "next/dynamic";
+import styled from "styled-components";
 import VaultNavigation from "../../components/vaultNavigation";
 
 const VaultView = dynamic(() => import("../../components/vaultView"), {
   ssr: false,
 });
 
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 1224px;
+  margin: 64px auto 0;
+  padding: 0 16px;
+  gap: 48px;
+
+  .nav-area {
+    width: 270px;
+  }
+
+  .content-area {
+    flex: 1;
+  }
+`;
+
 const Vault: NextPage = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        maxWidth: "1224px",
-        margin: "64px auto",
-        padding: "0 16px",
-        gap: "48px",
-      }}
-    >
-      <div style={{ width: "270px" }}>
+    <Wrapper>
+      <div className="nav-area">
         <VaultNavigation />
       </div>
-      <div style={{ flex: "1" }}>
+      <div className="content-area">
         <VaultView />
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
